d86c5b80cd7dfc3f1270e62662d0b9a15930fb6a
guga <guga@umich.edu>
2021-12-05 00:48:07 -0500

    Added IMU/Encoder move to target and turn.  Turn degrees needs conversion cal to be exact.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroBronco_Angle_Correct.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroBronco_Angle_Correct.java
@@ -7,13 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name = "GyroBronco")
-public class GyroBronco extends LinearOpMode {
+@Autonomous(name = "GyroBronco_AngleCorrect")
+public class GyroBronco_Angle_Correct extends LinearOpMode {
 
     private DcMotor front_left_motor;
     private DcMotor back_left_motor;
@@ -24,6 +25,8 @@ public class GyroBronco extends LinearOpMode {
 
     float xOrientation;
     float targetOrientationAngle;
+    Orientation lastAngles = new Orientation();
+    double globalAngle, power = 0.3, correction;
     //float encoderResolution= =((((1+(46/17))) * (1+(46/11))) * 28);
     //float wheelRevolution = (2*3.14159265359*95)/25.4 ;
 
@@ -64,12 +67,18 @@ public class GyroBronco extends LinearOpMode {
 //            telemetry.update();
             // To travel 24 in, we need = 2.02 x 537.7 pulses
             Move_to_Position(2.02 * 537.7);
+            sleep(250);
+            Turn_left_to_Position(1.5 * 537.7);
+            sleep(250);
+            Move_to_Position(2.02 * 537.7);
+            sleep(250);
+            Turn_right_to_Position(1.5 * 537.7);
             telemetry.addData("X axis angle ", getXAxisOrientation());
             telemetry.update();
-            // turn CW unti you reach 90
-            rotateCW(90);
-            telemetry.addData("X axis angle ", getXAxisOrientation());
-            telemetry.update();
+//            // turn CW unti you reach 90
+            //rotate(90, power);
+//            telemetry.addData("X axis angle ", getXAxisOrientation());
+//            telemetry.update();
 
 
         }
@@ -114,12 +123,82 @@ public class GyroBronco extends LinearOpMode {
         back_left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         back_right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // Turn on moters using moderate power
-        front_left_motor.setPower(0.3);
-        front_right_motor.setPower(0.3);
-        back_left_motor.setPower(0.3);
-        back_right_motor.setPower(0.3);
+//        front_left_motor.setPower(0.3);
+//        front_right_motor.setPower(0.3);
+//        back_left_motor.setPower(0.3);
+//        back_right_motor.setPower(0.3);
+        front_left_motor.setPower(power-correction);
+        back_left_motor.setPower(power-correction);
+        front_right_motor.setPower(power+correction);
+        back_right_motor.setPower(power+correction);
+
         // Loop until the motor reaches its target position
         while (front_left_motor.getCurrentPosition() < TargetPosition) {
+            // Nothing while the robot moves forward
+        }
+        front_left_motor.setPower(0);
+        front_right_motor.setPower(0);
+        back_left_motor.setPower(0);
+        back_right_motor.setPower(0);
+        // sleep quarter of seconds to let the robot sleep
+        sleep(250);
+    }
+
+    private void Turn_left_to_Position(double TargetPosition) {
+        // Reset the encoders
+        front_left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Put the motor in encoder mode
+        front_left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Turn on moters using moderate power
+        front_left_motor.setPower(0.3);
+        front_right_motor.setPower(-0.3);
+        back_left_motor.setPower(0.3);
+        back_right_motor.setPower(-0.3);
+//        front_left_motor.setPower(power-correction);
+//        back_left_motor.setPower(power-correction);
+//        front_right_motor.setPower(power+correction);
+//        back_right_motor.setPower(power+correction);
+
+        // Loop until the motor reaches its target position
+        while (front_left_motor.getCurrentPosition() < TargetPosition) {
+            // Nothing while the robot moves forward
+        }
+        front_left_motor.setPower(0);
+        front_right_motor.setPower(0);
+        back_left_motor.setPower(0);
+        back_right_motor.setPower(0);
+        // sleep quarter of seconds to let the robot sleep
+        sleep(250);
+    }
+    private void Turn_right_to_Position(double TargetPosition) {
+        // Reset the encoders
+        front_left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Put the motor in encoder mode
+        front_left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Turn on moters using moderate power
+        front_left_motor.setPower(-0.3);
+        front_right_motor.setPower(+0.3);
+        back_left_motor.setPower(-0.3);
+        back_right_motor.setPower(+0.3);
+//        front_left_motor.setPower(power-correction);
+//        back_left_motor.setPower(power-correction);
+//        front_right_motor.setPower(power+correction);
+//        back_right_motor.setPower(power+correction);
+
+        // Loop until the motor reaches its target position
+        while (front_right_motor.getCurrentPosition() < TargetPosition) {
             // Nothing while the robot moves forward
         }
         front_left_motor.setPower(0);
@@ -236,7 +315,7 @@ public class GyroBronco extends LinearOpMode {
         front_right_motor.setPower(-0.4);
         back_right_motor.setPower(-0.4);
         // loop until we reach the target orientation
-        while (xOrientation < 90) {
+        while (Math.abs(xOrientation) < 90) {
             // Update current orientation about X
             xOrientation = getXAxisOrientation();
         }
@@ -248,6 +327,122 @@ public class GyroBronco extends LinearOpMode {
         // wait a moment for the robot to stop
         sleep(250);
 
+    }
+
+    /**
+     * Resets the cumulative angle tracking to zero.
+     */
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
+    /**
+     * Get current cumulative angle rotation from last reset.
+     * @return Angle in degrees. + = left, - = right.
+     */
+    private double getAngle()
+    {
+        // We experimentally determined the X axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the X axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    /**
+     * See if we are moving in a straight line and if not return a power correction value.
+     * @return Power adjustment, + is adjust left - is adjust right.
+     */
+    private double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .10;
+
+        angle = getAngle();
+
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+
+        correction = correction * gain;
+
+        return correction;
+    }
+
+    /**
+     * Rotate left or right the number of degrees. Does not support turning more than 180 degrees.
+     * @param degrees Degrees to turn, + is left - is right
+     */
+    private void rotate(int degrees, double power)
+    {
+        double  leftPower, rightPower;
+
+        // restart imu movement tracking.
+        resetAngle();
+
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
+
+        if (degrees < 0)
+        {   // turn right.
+            leftPower = power;
+            rightPower = -power;
+        }
+        else if (degrees > 0)
+        {   // turn left.
+            leftPower = -power;
+            rightPower = power;
+        }
+        else return;
+
+        // set power to rotate.
+        front_left_motor.setPower(leftPower);
+        front_right_motor.setPower(rightPower);
+        back_left_motor.setPower(leftPower);
+        back_right_motor.setPower(rightPower);
+
+        // rotate until turn is completed.
+        if (degrees < 0)
+        {
+            // On right turn we have to get off zero first.
+            while (opModeIsActive() && getAngle() == 0) {}
+
+            while (opModeIsActive() && getAngle() > degrees) {}
+        }
+        else    // left turn.
+            while (opModeIsActive() && getAngle() < degrees) {}
+
+        // turn the motors off.
+        front_left_motor.setPower(0);
+        back_left_motor.setPower(0);
+        front_right_motor.setPower(0);
+        back_right_motor.setPower(0);
+
+        // wait for rotation to stop.
+        sleep(1000);
+
+        // reset angle tracking on new heading.
+        resetAngle();
     }
 
 
